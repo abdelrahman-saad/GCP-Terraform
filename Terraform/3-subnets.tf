e25574e8398a64d4eeb56b39334727f53caa40a2
Abdelrahman Saad <abdelrahman.a171@gmail.com>
2023-10-20 22:05:25 +0300

    make the subnets private

--- Terraform/3-subnets.tf
+++ Terraform/3-subnets.tf
@@ -1,15 +1,17 @@
 resource "google_compute_subnetwork" "management_subnet" {
-  name          = "management"
-  ip_cidr_range = var.first_cider
-  region        = var.used_region_1
-  network       = google_compute_network.application-vpc.id
+  name                      = "management"
+  ip_cidr_range             = var.first_cider
+  region                    = var.used_region_1
+  network                   = google_compute_network.application-vpc.id
+  private_ip_google_access  = true
 }
 
 
 resource "google_compute_subnetwork" "workload_subnet" {
-  name          = "workload"
-  ip_cidr_range = var.second_cider
-  region        = var.used_region_2
-  network       = google_compute_network.application-vpc.id
+  name                      = "workload"
+  ip_cidr_range             = var.second_cider
+  region                    = var.used_region_2
+  network                   = google_compute_network.application-vpc.id
+  private_ip_google_access  = true
 
 }
