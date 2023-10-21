f5fd66e8a982527f3b85cc41fbb6fb5e500ebd4b
Abdelrahman A. Saad <abdelrahman.a171@gmail.com>
2023-10-21 20:58:44 +0300

    fix path for secret file

--- Terraform/1-provider.tf
+++ Terraform/1-provider.tf
@@ -1,4 +1,4 @@
 provider "google" {
-  credentials = file("C:/Users/asaad/Desktop/gcp-terraform/GCP-Terraform/secrets/gcp-terraform-as.json")
+  credentials = file("../secrets/gcp-terraform-as.json")
   project     = var.project_id
 }
