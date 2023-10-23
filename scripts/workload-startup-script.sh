0f96d186b7bd412186c3e9220611db5f8babaa5f
Abdelrahman Saad <abdelrahman.a171@gmail.com>
2023-10-23 22:14:28 +0300

    editing the script and added a working image

--- scripts/workload-startup-script.sh
+++ scripts/workload-startup-script.sh
@@ -6,51 +6,6 @@ sudo apt-get upgrade -y
 # Install Docker and Git
 sudo apt-get install -y docker.io git
 
-# clone Node.js Project
-git clone https://github.com/Mostafa-Yehia/simple-node-app.git
-
-# Build a Docker image
-cd simple-node-app
-
-#####################################
-# docker script
-#####################################
-
-cat > Dockerfile << EOF
-# the image used Node which contains version 14 of Node and NPM
-FROM node:14
-
-WORKDIR /app
-
-# this file holds declaration for packages
-COPY package*.json ./
-
-# Install npm packages
-RUN npm install
-
-# Copy application files into the container
-COPY . .
-
-# Expose the port 3000 for node app to work
-ENV WEBport 3000
-
-# Set MongoDB user, password, and hosts
-ENV DBuser test
-ENV DBpass test123
-ENV DBhosts mongo-0.mongo:27017
-
-# Start your application
-CMD ["node", "index.js"]
-
-EOF
-
-###############################
-##### End of docker file
-###############################
-
-docker build -t node-app -f Dockerfile .
-
-
 
 cd /tmp
 
@@ -72,7 +27,11 @@ cat key.json | docker login -u _json_key_base64 --password-stdin \
 https://us-eastl-docker.pkg.dev
 
 
-sudo docker tag node-app "us-east1-docker.pkg.dev/gcp-terraform-as/gcp-terraform-as-repo/node-app:latest"
+# pull node.js app
+
+sudo docker pull moelshafei/nodeapp:latest
+
+sudo docker tag moelshafei/nodeapp:latest "us-east1-docker.pkg.dev/gcp-terraform-as/gcp-terraform-as-repo/node-app:latest"
 
 sudo docker push "us-east1-docker.pkg.dev/gcp-terraform-as/gcp-terraform-as-repo/node-app:latest"
 
